e98194fa46492de8fb13010e1988e5a9c09c0784
francescox001 <155748108+francescox001@users.noreply.github.com>
2024-03-01 17:43:04 +0100

    Add files via upload

--- 009-rotating-image-gallery/index.js
+++ 009-rotating-image-gallery/index.js
@@ -1,9 +1,24 @@
 const imageContainerEl = document.querySelector(".image-container");
 const prevEl = document.getElementById("prev");
 const nextEl = document.getElementById("next");
+const btnChangeEl = document.getElementById('changeImageBtn');
 
 let x = 0;
 let timer;
+
+btnChangeEl.addEventListener('click', function() {
+    // Seleziona tutti gli elementi <img> all'interno del contenitore
+    const images = document.querySelectorAll('.image-container img');
+
+    // Itera su ciascuna immagine
+    images.forEach(img => {
+        // Genera un nuovo ID casuale per l'immagine
+        const newId = Math.floor(Math.random() * 1000) + 1; // ID casuale tra 1 e 1000
+        // Aggiorna l'attributo src dell'immagine con il nuovo ID
+        img.src = `https://picsum.photos/id/${newId}/300/200`;
+    });
+});
+
 
 prevEl.addEventListener("click", () => {
     x += 45;
